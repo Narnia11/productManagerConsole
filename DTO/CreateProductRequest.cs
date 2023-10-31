bd7bf6515d81a08562428f1043bda0d3ad651906
Narnia11 <narges.hekmatnia1983@gmail.com>
2023-10-31 17:20:54 +0100

    Edit classes

--- DTO/CreateProductRequest.cs
+++ DTO/CreateProductRequest.cs
@@ -3,12 +3,8 @@ namespace ProductManager.DTO;
 public class CreateProductRequest
 {
     public string ProductName { get; set; }
-
     public string SerialNum { get; set; }
-
-    public string ProductDesc  { get; set; }
-
+    public string ProductDesc { get; set; }
     public string ImageUrl { get; set; }
-
     public int Price { get; set; }
 }
